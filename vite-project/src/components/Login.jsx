cfab708dc81a8a556fe7b09c40c8218f0283b68e
e-savar <66883712+e-savar@users.noreply.github.com>
2023-10-28 13:38:08 -0400

    some login

--- vite-project/src/components/Login.jsx
+++ vite-project/src/components/Login.jsx
@@ -1,7 +1,37 @@
+import TextField from '@mui/material/TextField';
 import React from 'react';
 
 const Login = () => {
-  return <div>Login Page Content</div>;
+  return (
+    <div>
+      <div style={styles.loginBox}>
+        <h2 style={styles.textcolor}>Login</h2>
+        <form>
+          <label style={styles.textcolor}>
+            Username:
+            <TextField id="outlined-basic" variant="outlined" />
+          </label>
+          <label style={styles.textcolor}>
+            Password:
+            <TextField id="outlined-basic" variant="outlined" />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  )
+};
+const styles = {
+  loginBox: {
+    width: '300px',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    backgroundColor: '#f9f9f9',
+  },
+  textcolor: {
+    color: 'black'
+  }
 };
 
 export default Login;
