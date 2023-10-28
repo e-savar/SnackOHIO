50636242af508c684d96ab5349d349290c87491d
Dan-Sted <79947537+Dan-Sted@users.noreply.github.com>
2023-10-28 18:52:39 -0400

    code

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
-
+import ImageSlider from "./ImageSlider";
 const Home = () => {
-  return <div>Home Page Content</div>;
+  const slides = [
+    { url: "https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80", title: "beach" },
+    { url: "https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80", title: "boat" },
+    { url: "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80", title: "forest" },
+    { url: "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80", title: "city" },
+    { url: "https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80", title: "italy" },
+  ];
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
+  return (
+    <div style={styles.mainPage}>
+        <div><h1>Products</h1></div>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
+    </div>
+  );
 };
+
+const styles = {
+    mainPage: {
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    textcolor: {
+      color: 'black'
+    }
+  };
+  
 
 export default Home;
