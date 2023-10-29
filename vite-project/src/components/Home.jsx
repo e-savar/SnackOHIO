0f97887b02cb8d06751c8eb06c2ba790eeb0b7ab
e-savar <66883712+e-savar@users.noreply.github.com>
2023-10-28 20:29:57 -0400

    here danny

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
     { url: "https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80", title: "italy" },
   ];
   const containerStyles = {
-    width: "500px",
-    height: "280px",
+    width: "42vw",
+    height: "42vh",
     margin: "0 auto",
   };
   return (
